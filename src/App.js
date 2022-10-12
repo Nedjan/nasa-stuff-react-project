2dcf91800e07b8c6877b955774cba572de50a322
Nedjan Shabani <nedjan.shabani@jebbit.com>
2022-10-12 09:33:33 +0200

    Moved main-container into route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Main from './routes/main';
 import Search from './routes/search';
 import Game from './routes/game';
 
 import './App.css';
 
-import Header from './containers/Header'
-import MainContainer from './containers/MainContainer'
+import Header from './containers/Header';
 
 export default function() {
   //the welcome component has the header/navbar and the button to choose to search is toggled
@@ -15,7 +15,7 @@ export default function() {
     <BrowserRouter>
       <div>
         <Header />
-        <Route exact path="/" component={MainContainer} />
+        <Route exact path="/" component={Main} />
         <Route exact path="/game" component={Game} />
         <Route exact path="/search" component={Search} />
       </div>
